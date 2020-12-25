f0ccb624ed52542515e0381a78351aff1a1ddcbc
Z1chi <55581497+Z1chi@users.noreply.github.com>
2020-12-25 15:11:29 +0300

    Add files via upload
    Без слайдера

--- js/script.js
+++ js/script.js
@@ -12,62 +12,26 @@ window.onload = function () {
 		document.getElementById("navigationMenu").classList.toggle("active");
 	}
 
-	let swiper = new Swiper('.swiper-container', {
-		spaceBetween: 10,
-		mousewheel: false,
-		clickable: true,
-		breakpoints: {
-			100: {
-				direction: 'vertical',
-				slidesPerView: 1,
-				slidesPerGroup: 1,
-				autoHeight: true,
-				loop: true,
-			},
-			566: {
-				direction: 'vertical',
-				slidesPerView: 2,
-				slidesPerGroup: 1,
-				loop: true,
-
-			},
-
-			830: {
-				direction: 'horizontal',
-				slidesPerGroup: 1,
-				slidesPerView: 3,
-				autoHeight: true,
-				loop: true,
-				pagination: {
-					dynamicBullets: true,
-					el: '.swiper-pagination',
-					clickable: true,
-					renderBullet: function (index, className) {
-						// console.log(index);
-						if (index >= 4) {
-
-						} else {
-							return '<span class="' + className + '">' + (index + 1) + '</span>';
-						}
-					},
-				},
-			},
-		}
-
-	});
-
 
 	let iframeVkApi = document.querySelector("#vkwidget1");
 	console.log(iframeVkApi);
 	let iframeDocument = iframeVkApi.contentDocument || iframeVkApi.contentWindow.document;
 	console.log(iframeDocument);
+	let iframeVkApiKek = async function (iframeVkApi) {
+		await console.log(iframeVkApi);
+		await console.log(iframeVkApi.contentDocument.querySelector(div));
+	};
 
-
-
-
+	let template = {
+		article :`<article class="oldSingleArticle">
+<img class="oldArticleImg" src="./img/ `${}`" alt="">
+<div>
+<a href="" class="oldSingleArticle_title"></a>
+<p class="oldSingleArticle_description"></p>
+</div>
+</article> `
+	}
 
 };
-let iframeVkApiKek = async function (iframeVkApi) {
-	await console.log(iframeVkApi);
-	await console.log(iframeVkApi.contentDocument.querySelector(div));
-};
+
+
