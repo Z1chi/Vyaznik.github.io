4a94698342a84b1ece33cddd08613cc2de4fd1a1
Z1chi <55581497+Z1chi@users.noreply.github.com>
2020-12-28 18:06:36 +0300

    Add files via upload
    Фикс разрешения картинок в статьях

--- js/script.js
+++ js/script.js
@@ -12,29 +12,23 @@ window.onload = function () {
 		document.getElementById("navigationMenu").classList.toggle("active");
 	}
 
-/*
-	let iframeVkApi = document.querySelector("#vkwidget1");
-	console.log(iframeVkApi);
-	let iframeDocument = iframeVkApi.contentDocument || iframeVkApi.contentWindow.document;
-	console.log(iframeDocument);
-	let iframeVkApiKek = async function (iframeVkApi) {
-		await console.log(iframeVkApi);
-		await console.log(iframeVkApi.contentDocument.querySelector('div'));
-	};*/
+	/*
+		let iframeVkApi = document.querySelector("#vkwidget1");
+		console.log(iframeVkApi);
+		let iframeDocument = iframeVkApi.contentDocument || iframeVkApi.contentWindow.document;
+		console.log(iframeDocument);
+		let iframeVkApiKek = async function (iframeVkApi) {
+			await console.log(iframeVkApi);
+			await console.log(iframeVkApi.contentDocument.querySelector('div'));
+		};*/
 
 
-
-
-
-
-
-		document.querySelector("#addOldArticle").addEventListener("click" , function () {
-			let linkImg = "./img/OldArticle1.png";
-			let linkTitle = "Название статьи Название статьи";
-			let articleText = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad mi";
-
-			let template = {
-				articleOld: `
+	document.querySelector("#addOldArticle").addEventListener("click", function () {
+		let linkImg = "./img/OldArticle1.png";
+		let linkTitle = "Название статьи Название статьи";
+		let articleText = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad mi";
+		let template = {
+			articleOld: `
 						<article class="oldSingleArticle">
 							<img class="oldArticleImg" src="{linkImg}" alt="">
 		<div>
@@ -42,16 +36,16 @@ window.onload = function () {
 		<p class="oldSingleArticle_description">{articleText}</p>
 		</div>
 		</article>`
-			};
+		};
 
-			let onceOldArticleGenerate =  template.articleOld
-				.replace("{linkImg}", linkImg)
-				.replace("{linkTitle}", linkTitle)
-				.replace("{articleText}", articleText);
+		let onceOldArticleGenerate = template.articleOld
+			.replace("{linkImg}", linkImg)
+			.replace("{linkTitle}", linkTitle)
+			.replace("{articleText}", articleText);
 
-			let areaInstall = document.querySelector(".add_OlderArticle");
-			areaInstall.innerHTML += 	onceOldArticleGenerate;
-		})
+		let areaInstall = document.querySelector(".add_OlderArticle");
+		areaInstall.innerHTML += onceOldArticleGenerate;
+	})
 
 };
 
